031207e4c001c8f575ea620b11c732e9527222f1
Dharavath Kailash <12029318@megha.ho>
2023-10-18 22:52:37 +0530

    conditional assignment

---
+++ Mathematical_Operator/p3.js
@@ -0,0 +1,8 @@
+let a=1
+let b=2
+
+if (a>b){
+  console.log("a is greater")
+}else{
+  console.log("b is greater")
+}
---
+++ Mathematical_Operator/p4.js
@@ -0,0 +1,14 @@
+//in data
+let cred_username="nani"
+let cred_password="chinni"
+//user i/p
+let username="nani"
+let password="chinni"
+//Backend
+if (cred_username==username){
+ if(cred_password==password){
+ console.log("sign in successfull")
+} else{
+ console.log("username/password is mismatch")
+}
+}
